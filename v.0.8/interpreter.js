39025c79ef015ab2b59fcf6365aaa9a0abd23b89
Conor O'Brien <conorobrien4god@gmail.com>
2015-11-10 20:29:23 -0500

    added future capacities, added commands I, n
    Added framework for infinite capacity numerics.

--- v.0.8/interpreter.js
+++ v.0.8/interpreter.js
@@ -1,3 +1,194 @@
+// version.0.8 commands
+const v0_8 = {
+	"A": function(s){
+		
+	},
+	"B": function(s){
+		
+	},
+	"C": function(s){
+		
+	},
+	"D": function(s){
+		
+	},
+	"E": function(s){
+		
+	},
+	"F": function(s){
+		
+	},
+	"G": function(s){
+		
+	},
+	"H": function(s){
+		
+	},
+	"I": function(s){
+		s.StripField[s.StripPointer].exec("+1");
+	},
+	"J": function(s){
+		
+	},
+	"K": function(s){
+		
+	},
+	"L": function(s){
+		
+	},
+	"M": function(s){
+		
+	},
+	"N": function(s){
+		
+	},
+	"O": function(s){
+		
+	},
+	"P": function(s){
+		
+	},
+	"Q": function(s){
+		
+	},
+	"R": function(s){
+		
+	},
+	"S": function(s){
+		
+	},
+	"T": function(s){
+		
+	},
+	"U": function(s){
+		
+	},
+	"V": function(s){
+		
+	},
+	"W": function(s){
+		
+	},
+	"X": function(s){
+		
+	},
+	"Y": function(s){
+		
+	},
+	"Z": function(s){
+		
+	},
+	"[": function(s){
+		
+	},
+	"\\": function(s){
+		
+	},
+	"]": function(s){
+		
+	},
+	"^": function(s){
+		
+	},
+	"_": function(s){
+		
+	},
+	"`": function(s){
+		
+	},
+	"a": function(s){
+		
+	},
+	"b": function(s){
+		
+	},
+	"c": function(s){
+		
+	},
+	"d": function(s){
+		
+	},
+	"e": function(s){
+		
+	},
+	"f": function(s){
+		this.safetyMode++;
+		if(this.safetyMode>2) this.safetyMode = 0;
+	},
+	"g": function(s){
+		
+	},
+	"h": function(s){
+		
+	},
+	"i": function(s){
+		
+	},
+	"j": function(s){
+		
+	},
+	"k": function(s){
+		
+	},
+	"l": function(s){
+		
+	},
+	"m": function(s){
+		
+	},
+	"n": function(s){
+		this.STDOUT(s.StripField[s.StripPointer].toString());
+	},
+	"o": function(s){
+		
+	},
+	"p": function(s){
+		
+	},
+	"q": function(s){
+		
+	},
+	"r": function(s){
+		
+	},
+	"s": function(s){
+		
+	},
+	"t": function(s){
+		
+	},
+	"u": function(s){
+		
+	},
+	"v": function(s){
+		
+	},
+	"w": function(s){
+		
+	},
+	"x": function(s){
+		
+	},
+	"y": function(s){
+		
+	},
+	"z": function(s){
+		
+	},
+	"{": function(s){
+		
+	},
+	"|": function(s){
+		
+	},
+	"}": function(s){
+		
+	},
+	"~": function(s){
+		
+	}
+}
+
 /*
  * Simplex - class
  * o Used to initiate a Simplex program (<code>)
@@ -5,20 +196,54 @@
  *   run at once
  */
 function Simplex(code){
+	this.code = code;
 	this.Macros       = [];	// to hold the macros
 	this.StripField   = [new Strip()];	// to hold the strips
+	this.inputStack   = [];
+	this.lambdaStack  = [];
 	this.StripPointer = 0;
-	this.index = 0;
-	this.step = 0;
-	this.commands = {
-		"A": function(s){
-			s.
-		}
+	this.index        = 0;
+	this.stepNum      = 0;
+	this.safetyMode   = true;
+	this.commands     = v0_8;
+}
+
+Simplex.prototype.STDERR = function(error){
+	
+}
+
+Simplex.prototype.STDOUT = function(msg){
+	alert(msg);
+}
+
+Simplex.prototype.STDIN = function(feed){
+	
+}
+
+Simplex.prototype.disp = function(){
+	return JSON.stringify(this.curStrip().strip.map(x=>x.toString()));
+}
+
+Simplex.prototype.curStrip = function(){
+	return this.StripField[this.StripPointer];
+}
+
+function complex(v){
+	if(v==parseInt(v)) return {real:+v,imag:0};
+	try {
+		var pm = /\+|-/;
+		var r=v.replace(/i/,"").split(pm);
+		if(r.length==2)
+			return {real:+r[0],imag:parseInt(v.match(pm)[0]+r[1],10)};
+		else if(r.length==1)
+			return {real:v.search(/i/)+1?0:+r[0],imag:v.search(/i/)+1?+r[0]:0};
+	} catch(e){
+		throw new Error("Invalid complex number: "+v);
 	}
 }
 
 Simplex.prototype.getCommand = function(f){
-	if(+f==f) return function(s){
+	if(f==parseInt(f)) return function(s){
 		s.StripField[s.StripPointer].write(+f);
 	}
 	if(this.commands[f]) return this.commands[f];
@@ -26,13 +251,20 @@ Simplex.prototype.getCommand = function(f){
 }
 
 Simplex.prototype.step = function(){
-	
+	try {
+	var chr = this.code[this.index];
+	var f = this.getCommand(chr);
+	if(f) f(this);
+	this.index++;
+	} catch(e){
+		console.log(e);
+	}
 }
 
 // to hold the cells
 function Strip(){
 	this.strip = [new Cell()];
-	this.poiner = 0;
+	this.pointer = 0;
 }
 
 Strip.prototype.activeCells = function(){
@@ -42,11 +274,12 @@ Strip.prototype.activeCells = function(){
 }
 
 Strip.prototype.get = function(index){
+	index = index||this.pointer;
 	return this.strip[index];
 }
 
-Strip.prototype.write = function(value){
-	this.strip[index]
+Strip.prototype.write = function(value,ind){
+	this.strip[(ind||this.pointer)].write(complex(value));
 }
 
 Strip.prototype.getVal = function(index,getImag){
@@ -55,40 +288,46 @@ Strip.prototype.getVal = function(index,getImag){
 	return [this.strip[index].real,this.strip[index].imag];
 }
 
-function BigNumber(num){
-	this.integer = Math.floor(num);
-	this.decimal = (num - this.integer).toString().split("");
-	this.integer = this.integer.toString().split("").reverse();
-	this.decimal.shift();
+Strip.prototype.exec = function(str,index){
+	eval("this.write((this.get("+index+").real"+str+")+\"+\"+(this.get("+index+").real+\"i\"));");
 }
 
-BigNumber.prototype.valueOf = function(){
-	return +(this.integer.reverse().concat(["."].concat(this.decimal))).join("");
-}
+{ // wip
+	function BigNumber(num){
+		this.integer = Math.floor(num);
+		this.decimal = (num - this.integer).toString().split("");
+		this.integer = this.integer.toString().split("").reverse();
+		this.decimal.shift();
+	}
 
-BigNumber.prototype.checkFlow = function(){
-	for(var i=0;i<this.integer.length;i++){
-		if(this.integer[i]>10){
-			this.integer[i] -= 10;
-			this.integer[i+1] = (this.integer[i+1]||0)+1;
-		} else if(this.integer[i]<0){
-			this.integer[i]+=10;
-			this.integer[i+1]-=1;
+	BigNumber.prototype.valueOf = function(){
+		return +(this.integer.reverse().concat(["."].concat(this.decimal))).join("");
+	}
+
+	BigNumber.prototype.checkFlow = function(){
+		for(var i=0;i<this.integer.length;i++){
+			if(this.integer[i]>10){
+				this.integer[i] -= 10;
+				this.integer[i+1] = (this.integer[i+1]||0)+1;
+			} else if(this.integer[i]<0){
+				this.integer[i]+=10;
+				this.integer[i+1]-=1;
+			}
 		}
 	}
-}
 
-BigNumber.prototype.add = function(num){
-	var minLen = Math.min(num.int.length,this.integer.length);
-	for(i=0;i<minLen;i++){
-		this.integer[i] += num.int[i];
+	BigNumber.prototype.add = function(num){
+		var minLen = Math.min(num.int.length,this.integer.length);
+		for(i=0;i<minLen;i++){
+			this.integer[i] += num.int[i];
+		}
+		this.checkFlow();
 	}
-	this.checkFlow();
+
+	var a = new BigNumber(345.76);
 }
 
-var a = new BigNumber(345.76);
-
-function Cell(real,imag){
+function Cell(real,imag,base){
 	this.real    = real || 0;
 	this.imag    = imag || 0;
 	this.base    = base || 10;
